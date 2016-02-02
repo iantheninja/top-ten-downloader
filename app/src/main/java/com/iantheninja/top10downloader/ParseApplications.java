83e07fcffc831c9e63a582c5f321628d63dd4f6a
iantheninja <munene.ian@live.com>
2016-02-02 12:13:59 +0300

    xml data being sorted by entry until end of document

---
+++ app/src/main/java/com/iantheninja/top10downloader/ParseApplications.java
@@ -0,0 +1,65 @@
+package com.iantheninja.top10downloader;
+
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
+import java.util.ArrayList;
+
+/**
+ * Created by ian on 01/02/16.
+ */
+public class ParseApplications {
+    private String xmlData;
+    private ArrayList<Application> applications;
+
+    public ParseApplications(String xmlData) {
+        this.xmlData = xmlData;
+        this.applications = new ArrayList<Application>();
+    }
+
+    public ArrayList<Application> getApplications() {
+        return applications;
+    }
+
+    public boolean process(){
+        boolean status = true;
+        Application currentRecord;
+        boolean inEntry = false;
+        String textValue = "";
+
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();//enables the pulling of large xml data streams
+            factory.setNamespaceAware(true);
+            XmlPullParser xmlPullParser = factory.newPullParser();//for deriving the little bits of data from from parser factory
+            xmlPullParser.setInput(new StringReader(this.xmlData));//gets an input stream (data being recieved)
+            int eventType = xmlPullParser.getEventType();// to know where theres an entry in the xml
+
+            while(eventType != XmlPullParser.END_DOCUMENT){
+                String tagName = xmlPullParser.getName(); //gets the name of the tag
+                switch (eventType){
+                    case XmlPullParser.START_TAG:
+                        Log.d("ParseApplications", "Starting tag for " + tagName);
+                        if (tagName.equalsIgnoreCase("entry")){
+                            inEntry = true; // entry encountered
+                            currentRecord = new Application(); //denotes a new application in the list
+                            break;
+                        }
+                    case XmlPullParser.END_TAG:
+                        Log.d("ParseApplications", "Starting tag for " + tagName);
+                        break;
+                    default:
+                        // functionality to be added later
+                }
+                eventType = xmlPullParser.next(); // continues to the next section
+            }
+
+        }catch (Exception e){
+            status = false;
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
